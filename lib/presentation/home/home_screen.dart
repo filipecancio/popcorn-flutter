1cd1a64c194e16f09af7437aaff6263e6eb20a2c
cancio <filipe.cancio@gmail.com>
2024-05-16 19:20:21 -0300

    setting home

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,14 +1,10 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:popcorn/components/molecules/toggle_button.dart';
 import 'package:popcorn/components/molecules/web_header.dart';
-import 'package:popcorn/model/movie_list_type.dart';
 import 'package:popcorn/presentation/home/home_store.dart';
+import 'dart:math' as math;
 
 import '../../components/atom/ItemList.dart';
-import '../../components/molecules/search_input.dart';
 import '../../model/movie.dart';
 import '../../repository/pop_repository.dart';
 
@@ -35,30 +31,47 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: WebHeader(
-        type: store.pageType,
-        onCLick: store.updateMovieType,
-        onChanged: (value) {
-          store.getFindMovie(value);
-        }
-      ),
+          type: store.pageType,
+          onCLick: store.updateMovieType,
+          onChanged: (value) {
+            store.getFindMovie(value);
+          }),
       //appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary, title: const Text("Popcorn"),),
       body: Center(
         child: Observer(builder: (_) {
-          return Column(
-            children: [
-              Text(store.pageTitle),
-              Expanded(
-                  child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, // Define the number of columns
-                ),
-                itemCount: store.movieList.length,
-                itemBuilder: (context, index) {
-                  return ItemList(movie: store.movieList[index]);
-                },
-              ))
-            ],
-          );
+          return LayoutBuilder(builder: (context,constraints){
+            double maxWidth =  math.min(constraints.maxWidth * 0.8, 1000.0);
+            return SizedBox(
+              width: maxWidth,
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    child: Padding(
+                      padding: const EdgeInsets.all(24),
+                      child: Text(store.pageTitle,
+                          style: const TextStyle(
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Montserrat',
+                              color: Color(0xFF5F2832))),
+                    ),
+                  ),
+                  Expanded(
+                      child: GridView.builder(
+                        gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 4, // Define the number of columns
+                        ),
+                        itemCount: store.movieList.length,
+                        itemBuilder: (context, index) {
+                          return ItemList(movie: store.movieList[index]);
+                        },
+                      ))
+                ],
+              )
+            );
+          });
         }),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
